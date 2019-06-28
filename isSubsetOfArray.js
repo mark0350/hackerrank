48c672a8aa96e02cdeb6ce05f3e61c9e10cb6f20
Mark <kaomask@gmail.com>
2019-06-28 17:00:40 -0400

    check indiscontinous subset of array

--- isSubsetOfArray.js
+++ isSubsetOfArray.js
@@ -1,5 +1,5 @@
 /**
- * check whether array sub is a subset of array a
+ * check whether an array is a consecutive subset of the other array
  */
 function isSubsetOfArray(sub, a) {
   const subLen = sub.length;
@@ -21,6 +21,28 @@ function isSubsetOfArray(sub, a) {
   return false;
 }
 
+function isDiscontinuousSubset(sub, a) {
+  let j = 0;
+  for (let i = 0; i < sub.length; i++) {
+    // check if sub array has been already longer than original array
+    if (a.length - j < sub.length - i) {
+      return false;
+    }
+    while (true) {
+      if (sub[i] === a[j]) {
+        j++;
+        break;
+      }
+      if (++j >= a.length) {
+        return false;
+      }
+
+    }
+  }
+
+  return true;
+}
+
 const testCase = [
   [[1, 2, 3, 4], [1, 2, 3, 4]],
   [[1, 2, 3], [1, 2, 3, 4]],
@@ -29,9 +51,16 @@ const testCase = [
   [[3, 4], [1, 2, 3]],
   [[1], [2]],
   [[1, 2], [2]],
-  [[2, 2, 2, 2], [2, 2, 2]]
+  [[2, 2, 2, 2], [2, 2, 2]],
+  [[1, 7, 9], [1, 9, 7, 6]],
+  [[1, 7, 9], [1, 3, 1, 7, 6, 9]],
+  [[1, 7, 9], [9, 1, 7, 1, 7, 10, 9, 11, 14]],
 ];
 
 testCase.forEach(v => {
-  console.log(isSubsetOfArray(...v));
+  console.log(`${v[0]} is${isSubsetOfArray(...v) ? ' ' : ' not '}subset of ${v[1]}`);
+});
+console.log('isDiscontinuousSubset start');
+testCase.forEach(v => {
+  console.log(`${v[0]} is${isDiscontinuousSubset(...v) ? ' ' : ' not '}subset of ${v[1]}`);
 });
